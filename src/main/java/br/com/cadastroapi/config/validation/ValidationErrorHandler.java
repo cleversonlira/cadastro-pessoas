60075e758f4d8ea569c034ca1e5817a259ebdb72
cleversonlira <cleverson_lira@outlook.com>
2021-09-27 22:19:35 -0300

    reformulando mensagens de erro. Testes via postman

--- src/main/java/br/com/cadastroapi/config/validation/ValidationErrorHandler.java
+++ src/main/java/br/com/cadastroapi/config/validation/ValidationErrorHandler.java
@@ -6,8 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -16,6 +19,9 @@ public class ValidationErrorHandler {
 	@Autowired
 	private MessageSource messageSource;
 	
+
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public List<Erro> handle(MethodArgumentNotValidException exception) {
 		List<Erro> formErrors = new ArrayList<>();
 		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
