183f3cf08c7c5ceee704d65798ea755e5618c643
cleversonlira <cleverson_lira@outlook.com>
2021-09-30 23:25:23 -0300

    Corrigindo Bug - Recriando PessoaController

---
+++ src/main/java/br/com/cadastroapi/controller/PessoaController.java
@@ -0,0 +1,80 @@
+package br.com.cadastroapi.controller;
+
+import java.net.URI;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.cadastroapi.controller.dto.PessoaDTO;
+import br.com.cadastroapi.controller.form.PessoaForm;
+import br.com.cadastroapi.modelo.Pessoa;
+import br.com.cadastroapi.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+	
+	@Autowired
+	private PessoaRepository repository;
+		
+	@GetMapping
+	public Page<PessoaDTO> listar(Pageable paginacao) {
+		return PessoaDTO.converterLista(repository.findAll(paginacao));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PessoaDTO> buscarPorId(@PathVariable Long id) {
+		Optional<Pessoa> pessoa = repository.findById(id);
+		if (pessoa.isPresent()) {
+			return ResponseEntity.ok(new PessoaDTO(pessoa.get()));			
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<PessoaDTO> criar(@RequestBody @Valid PessoaForm form, UriComponentsBuilder uriBuilder) {
+		Pessoa pessoa = form.converter();
+		repository.save(pessoa);
+		URI uri = uriBuilder.path("/pessoas/{id}").buildAndExpand(pessoa.getId()).toUri();
+		return ResponseEntity.created(uri).body(new PessoaDTO(pessoa));
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<PessoaDTO> atualizar(@PathVariable Long id, @RequestBody @Valid PessoaForm form) {
+		Optional<Pessoa> pessoa = repository.findById(id);
+		if (pessoa.isPresent()) {
+			form.atualizar(pessoa.get());
+			return ResponseEntity.ok(new PessoaDTO(pessoa.get()));			
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<PessoaDTO> deletar(@PathVariable Long id) {		
+		Optional<Pessoa> pessoa = repository.findById(id);
+		if (pessoa.isPresent()) {
+			repository.deleteById(id);
+			return ResponseEntity.ok().build();			
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+}
